c80181b0e81ab94b59addbc02ca8d9c91a5a9862
John Flinchbaugh <john@hjsoft.com>
2025-12-01 11:24:42 -0500

    bootstrap cleanup

--- src/aoc2025/core.clj
+++ src/aoc2025/core.clj
@@ -1,44 +1,29 @@
 ;; ---------------------------------------------------------
 ;; aoc2025.core
-;;
-;; TODO: Provide a meaningful description of the project
 ;; ---------------------------------------------------------
 
 (ns aoc2025.core
   (:gen-class)
   (:require
-   [com.brunobonacci.mulog :as mulog]))
+   [com.brunobonacci.mulog :as log]))
 
 ;; ---------------------------------------------------------
 ;; Application
-
-(defn greet
-  "Greeting message via Clojure CLI clojure.exec"
-  ([] (greet {:team-name "secret engineering"}))
-  ([{:keys [team-name]}]
-   (str "aoc2025 core service developed by the " team-name " team")))
-
 
 (defn -main
   "Entry point into the application via clojure.main -M"
   [& args]
   (let [team (first args)]
-    (mulog/set-global-context!
+    (log/set-global-context!
      {:app-name "aoc2025 core" :version  "0.1.0-SNAPSHOT"})
-    (mulog/log ::application-starup :arguments args)
+    (log/log ::application-startup :arguments args)
     (if team
-      (println (greet team))
-      (println (greet)))))
+      (println "hello")
+      (println (str "hello " team)))))
 
 ;; ---------------------------------------------------------
 
 
-;; ---------------------------------------------------------
-;; Rick Comment
 (comment
 
-  (-main)
-  (-main {:team-name "Clojure Engineering"})
-
-  #_()) ; End of rich comment block
-;; ---------------------------------------------------------
+  nil)
--- test/aoc2025/core_test.clj
+++ test/aoc2025/core_test.clj
@@ -14,14 +14,6 @@
    [clojure.test :refer [deftest is testing]]
    [aoc2025.core :as core]))
 
-
 (deftest application-test
-  (testing "TODO: Start with a failing test, make it pass, then refactor"
-
-    ;; TODO: fix greet function to pass test
-    (is (= "aoc2025 application developed by the secret engineering team"
-           (core/greet)))
-
-    ;; TODO: fix test by calling greet with {:team-name "Practicalli Engineering"}
-    (is (= (core/greet "Practicalli Engineering")
-           "aoc2025 service developed by the Practicalli Engineering team"))))
+  (testing "test"
+    (is (= 1 1) "yay, math")))

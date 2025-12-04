5bba512bf185c7a0044134eb16baa38c81b1bbfd
John Flinchbaugh <john@hjsoft.com>
2025-12-03 21:52:24 -0500

    day 3, part 2

--- src/aoc2025/day_3.clj
+++ src/aoc2025/day_3.clj
@@ -1,36 +1,68 @@
 (ns aoc2025.day-3
   (:require [clojure.string :as str]
-            [clojure.math.combinatorics :as combo]))
+            [clojure.math.combinatorics :as combo]
+            [com.brunobonacci.mulog :as mu]))
 
 (defn ordered-combos
   "produce all combinations but retain duplicate values,
   unlike combinatorics/combinations"
-  [lst c]
-  (->
-    (map (fn [i v] [i v]) (range) lst)
-    (combo/combinations 2)
-    (->> (map (fn [[[_ a] [_ b]]] [a b])))))
+  [c lst]
+  (if (not (seq lst))
+    []
+    (->
+      (map list (range) lst) ; introduce indices to make every value unique
+      (combo/combinations c)
+      (->> (map (fn [lst] (map second lst)))))))
 
-(defn find-max [digits]
+(defn pair-down-max
+  "pair down a list of digits to produce n digits to make the largest number"
+  ([n digits]
+   (pair-down-max [] n digits))
+
+  ([selected n digits]
+  (if (<= n (count selected))
+    selected
+    (let [front (drop-last (- n (count selected) 1) digits)
+          end (take-last (- n (count selected) 1) digits)
+          largest (->> front (reduce max 0))]
+      (recur
+       (conj selected largest)
+       n
+       (concat
+        (rest (drop-while (partial not= largest) front))
+        end))))))
+
+(defn find-max [n digits]
   (->>
-    (ordered-combos digits 2)
-    (map (fn [c] (parse-long (apply str c))))
-    sort
-    reverse
-    first
-    ))
+   digits
+   (map (comp parse-long str))
+   (pair-down-max n)
+   (apply str)
+   parse-long))
 
 (defn part-1 [input]
   (->> input
-    str/split-lines
-    (map find-max)
-    (reduce + 0)))
+       str/split-lines
+       (map (partial find-max 2))
+       (reduce + 0)))
+
+(defn part-2 [input]
+  (->> input
+       str/split-lines
+       (map (partial find-max 12))
+       (reduce + 0)))
 
 (comment
   (->
-    "input/day-3.txt"
-    slurp
-    part-1)
+   "input/day-3.txt"
+   slurp
+   part-1)
   ;; => 17100
+
+  (->
+   "input/day-3.txt"
+   slurp
+   part-2)
+  ;; => 170418192256861
 
   nil)
--- test/aoc2025/day_3_test.clj
+++ test/aoc2025/day_3_test.clj
@@ -11,20 +11,45 @@
 "))
 
 (t/deftest test-ordered-combos
-  (t/is (=
-         [[10 20] [10 30] [10 10] [20 30] [20 10] [30 10]]
-         (sut/ordered-combos [10 20 30 10] 2))))
+  (t/are
+   [expected n lst]
+   (= expected (sut/ordered-combos n lst))
+    [[10 20] [10 30] [10 10] [20 30] [20 10] [30 10]] 2 [10 20 30 10]
+    [[10 20 30] [10 20 10] [10 30 10] [20 30 10]] 3 [10 20 30 10]))
 
 (t/deftest test-find-max
   (t/are
-   [expected input]
-   (= expected (sut/find-max input))
-   89 "89"
-   89 "189"
-   89 "819"
-   82 "812"
-   82 "2812"
-   98 "8918"))
+   [expected n input]
+   (= expected (sut/find-max n input))
+    89 2 "89"
+    89 2 "189"
+    89 2 "819"
+    82 2 "812"
+    82 2 "2812"
+    98 2 "8918"
+    92 2 "818181911112111"
+    918 3 "8911118"))
+
+(t/deftest test-pair-down-max
+  (t/are
+   [expected n input]
+      (=
+        expected
+        (parse-long
+          (apply str
+            (sut/pair-down-max n
+              (map (comp parse-long str) input)))))
+    89 2 "89"
+    89 2 "189"
+    89 2 "819"
+    82 2 "812"
+    82 2 "2812"
+    98 2 "8918"
+    92 2 "818181911112111"
+    918 3 "8911118"))
 
 (t/deftest test-part-1
   (t/is (= 357 (sut/part-1 input))))
+
+(t/deftest test-part-2
+  (t/is (= 3121910778619 (sut/part-2 input))))

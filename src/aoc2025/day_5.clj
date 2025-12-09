692f32cbaf7cf1a2957be5df4ef6d81c3b0cc662
John Flinchbaugh <john@hjsoft.com>
2025-12-09 00:20:23 -0500

    day 5 part 2

--- src/aoc2025/day_5.clj
+++ src/aoc2025/day_5.clj
@@ -1,31 +1,72 @@
 (ns aoc2025.day-5
   (:require [aoc2025.core :refer :all]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [com.brunobonacci.mulog :as mu]))
 
 (defn part-1 [input]
   (let [[fresh _ ingredients] (->>
-                             input
-                             str->lines
-                             (partition-by #{""}))
+                               input
+                               str->lines
+                               (partition-by #{""}))
         fresh-checks (->>
-                       fresh
-                       (map
-                         (fn [i]
-                           (let [[start end] (str/split i #"-")]
-                             (sort [(parse-long start) (parse-long end)])))))
+                      fresh
+                      (map
+                       (fn [i]
+                         (let [[start end] (str/split i #"-")]
+                           (sort [(parse-long start) (parse-long end)])))))
         ingredients (map parse-long ingredients)]
     (->>
-      ingredients
-      (filter
-        (fn [i] (some (fn [[start end]] (<= start i end)) fresh-checks))
-        )
-      count)))
+     ingredients
+     (filter
+      (fn [i] (some (fn [[start end]] (<= start i end)) fresh-checks)))
+     count)))
+
+(defn merge-ranges
+  [[s1 e1 :as r1] [s2 e2 :as r2]]
+  (cond
+    (<= s1 s2 e2 e1) [r1]
+    (<= s2 s1 e1 e2) [r2]
+    (<= s1 s2 e1 e2) [[s1 e2]]
+    (<= s2 s1 e2 e1) [[s2 e1]]
+    :else [r1 r2]))
+
+(defn merge-all-ranges [ranges r]
+  (let [[current & done] ranges]
+    (cond
+      (nil? current) [r]
+      :else (concat (reverse (merge-ranges current r)) done))))
+
+(defn part-2 [input]
+  (let [fresh (->>
+               input
+               str->lines
+               (partition-by #{""})
+               first)
+        fresh-checks (->>
+                      fresh
+                      (map
+                       (fn [i]
+                         (let [[start end] (str/split i #"-")]
+                           (vec (sort [(parse-long start) (parse-long end)])))))
+                      sort)]
+    (->>
+      fresh-checks
+      (reduce merge-all-ranges [])
+      (map (fn [[s e]] (inc (- e s))))
+      (reduce + 0))))
 
 (comment
   (->>
    "input/day-5.txt"
    slurp
    part-1)
-  ;; => 674
+;; => 674
+
+
+  (->>
+    "input/day-5.txt"
+    slurp
+    part-2)
+  ;; => 352509891817881
 
   )
--- test/day_5_test.clj
+++ test/day_5_test.clj
@@ -18,3 +18,26 @@
 
 (t/deftest test-part-1
   (t/is (= 3 (sut/part-1 input))))
+
+(t/deftest test-merge-ranges
+  (t/are
+   [expected r1 r2]
+   (= expected (sut/merge-ranges r1 r2))
+    [[1 2] [4 5]] [1 2] [4 5] ; non-intersecting
+    [[1 6]] [1 3] [3 6] ; overlapping end merges
+    [[1 6]] [1 6] [2 3] ; first contains second
+    [[1 6]] [1 6] [5 6] ; first contains second at end
+    [[1 6]] [1 3] [2 6] ; merge: start first, end second
+    [[1 6]] [2 6] [1 5] ; merge: start second, end first
+    ))
+
+(t/deftest test-merge-all-ranges
+  (t/are
+   [expected a r]
+   (= expected (sut/merge-all-ranges a r))
+   [[1 2]] [] [1 2]
+   [[1 4]] [[1 4]] [2 3]
+   [[6 7] [1 4]] [[1 4]] [6 7]))
+
+(t/deftest test-part2
+  (t/is (= 14 (sut/part-2 input))))

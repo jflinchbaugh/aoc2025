150151bc1eb6786dfd347c5851d6728a7e888962
John Flinchbaugh <john@hjsoft.com>
2025-12-10 09:24:25 -0500

    day 6 part 2

--- src/aoc2025/day_6.clj
+++ src/aoc2025/day_6.clj
@@ -1,5 +1,6 @@
 (ns aoc2025.day-6
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [com.brunobonacci.mulog :as mu]))
 
 (defn calc [[operator & operands]]
   (apply ({ "+" + "*" *} operator) (map parse-long operands)))
@@ -23,24 +24,26 @@
              calc)))
     (reduce + 0)))
 
+(defn recompose [i]
+  (mu/log ::recompose :i i)
+  (let [operator (str (last (first i)))
+        operands (map (fn [i'] (->> i'
+                                 (filter (complement #{\* \+ \space}))
+                                 str/join)) i)]
+    (cons operator operands)))
+
 (defn part-2 [input]
   (->>
     input
-    str/trim
     str/split-lines
-    (map
-      (fn [s]
-        (->
-          s
-          str/trim
-          (str/split #" +"))))
+    (remove str/blank?)
     (apply map list)
-    (map (fn [c]
-           (->>
-             c
-             reverse
-             calc)))
-    (reduce + 0)))
+    (partition-by (partial every? #{\space}))
+    (remove (fn [c] (every? #{\space} (first c))))
+    (map recompose)
+    (map calc)
+    (reduce + 0)
+    ))
 
 (comment
 
@@ -49,5 +52,11 @@
     part-1
     )
   ;; => 4580995422905
+
+  (-> "input/day-6.txt"
+    slurp
+    part-2
+    )
+  ;; => 10875057285868
 
   nil)
